93bd7cb9de9246770e1f5585fc41499071c76cac
Malin Hanak <malinehanak@gmail.com>
2019-01-24 09:53:02 +0100

    Delete Item test

--- server/schema/model/item.test.js
+++ server/schema/model/item.test.js
@@ -1,32 +1,49 @@
 import Item from './item';
 
 jest.mock('axios');
-let mock;
+let mockFn;
 let resp;
 
 beforeEach(() => {
-  mock = jest.fn().mockImplementation(() => Promise.resolve(resp))
+  mockFn = jest.fn().mockImplementation(() => Promise.resolve(resp))
 });
+
+test('Should delete an item', () => {
+  resp = {data: [{id: 3}]};
+
+  Item.api = {
+    delete: mockFn
+  }
+
+
+  return Item.delete(null)
+  .then(item => {
+    console.log(item)
+    expect(mockFn.mock.calls.length).toBe(1)
+    expect(item.id).toBeNull()
+  })
+});
+
 
 test('Should create an item', () => {
   resp = {data: [{name: "item1", description: "item one is awesome", quantity: "3", prize: "60", category: "RIDER"}]};
 
   Item.api = {
-    post: mock
+    post: mockFn
   }
 
   return Item.create({name: "item1", description: "item one is awesome", quantity: "3", prize: "60", category: "RIDER"})
   .then(item => {
-    expect(mock.mock.calls[0][0]).toEqual("/items")
-    expect(mock.mock.calls[0][1]).toEqual(resp.data[0])
-    expect(mock.mock.calls.length).toBe(1)
+    expect(mockFn.mock.calls[0][0]).toEqual("/items")
+    expect(mockFn.mock.calls[0][1]).toEqual(resp.data[0])
+    expect(mockFn.mock.calls.length).toBe(1)
   })
 });
 
 test('Should update item', () => {
   resp = {data: [{id: 3, name: "item1", description: "item one is awesome", quantity: "3"}]};
   Item.api = {
-    patch: mock
+    patch: mockFn
   }
 
   return Item.update(3, {name: "item1", description: "item one is awesome, yaaay", quantity: "3"})
