efe0437dd01cfe62b6e61ec5fdf93cda8e63e3eb
Malin Hanak <malinehanak@gmail.com>
2019-01-26 18:12:04 +0100

    Basic UI

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -8,14 +8,17 @@ class Home extends Component {
     return (
       <div className="basic-grid">
         <div className="top-level">
-          top level
+          <h1>Horse Galore</h1>
         </div>
         <Navigation />
         <div className="main-content">
           {this.props.children}
         </div>
         <div className="footer">
-          footer
+          <p>Horse Galore, copyright 2019</p>
+        </div>
+        <div className="side-nav">
+          S
         </div>
       </div>
       
