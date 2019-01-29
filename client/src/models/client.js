340d60c31ffa25ef588bef19f02ce6c2a1267c00
Malin Hanak <malinehanak@gmail.com>
2019-01-29 13:40:34 +0100

    possibly new client

--- client/src/models/client.js
+++ client/src/models/client.js
@@ -1,26 +1,56 @@
 import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+// import { onError } from 'apollo-link-error';
+// import { ApolloLink } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
 import { HttpLink } from 'apollo-link-http';
-import { onError } from 'apollo-link-error';
-import { ApolloLink } from 'apollo-link';
+import { split } from 'apollo-link';
+import { getMainDefinition } from 'apollo-utilities';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const wsLink = new WebSocketLink({
+  uri: "ws://localhost:8000/graphql",
+  options: {
+    reconnect: true
+  }
+});
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:8000/graphql',
+  credentials: 'same-origin'
+});
+
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLink,
+);
 
 const client = new ApolloClient({
-  link: ApolloLink.from([
-    onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors)
-        graphQLErrors.map(({ message, locations, path }) =>
-          console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-          ),
-        );
-      if (networkError) console.log(`[Network error]: ${networkError}`);
-    }),
-    new HttpLink({
-      uri: 'http://localhost:8000/graphql',
-      credentials: 'same-origin'
-    })
-  ]),
+  link,
   cache: new InMemoryCache()
-});
+})
+
+// const client = new ApolloClient({
+//   link: ApolloLink.from([
+//     onError(({ graphQLErrors, networkError }) => {
+//       if (graphQLErrors)
+//         graphQLErrors.map(({ message, locations, path }) =>
+//           console.log(
+//             `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+//           ),
+//         );
+//       if (networkError) console.log(`[Network error]: ${networkError}`);
+//     }),
+//     new HttpLink({
+//       uri: 'http://localhost:8000/graphql',
+//       credentials: 'same-origin'
+//     })
+//   ]),
+//   cache: new InMemoryCache()
+// });
 
 export default client;
