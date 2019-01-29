e23f1df8580e7e0d0aa7d7a5c99f6006c6df15d7
Malin Hanak <malinehanak@gmail.com>
2019-01-29 16:14:59 +0100

    Refactoring WebShop.js
    Moved out the map that lists the items in the shop.
    
    Refactored the subscriptions, so they are in a function to be called.

---
+++ client/src/components/shop/ListItem.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+
+class WebShop extends Component {
+  getCategory = (item) => {
+    let Cat = '';
+    if(item.category === 'RIDER'){Cat = 'Ryttare'}
+    else if (item.category === 'HORSE'){Cat = 'Häst'}
+    else { Cat = 'Övrigt'}
+    return Cat
+  }
+  render() {
+    const items = this.props.list.map((item) => {
+      return (
+        <TableRow key={item.EAN}>
+          <TableCell>{item.name}</TableCell>
+          <TableCell align="left">{item.color}</TableCell>
+          <TableCell align="left">{item.description}</TableCell>
+          <TableCell align="center">{item.quantity}</TableCell>
+          <TableCell align="center">{item.prize}</TableCell>
+           <TableCell align="right">{this.getCategory(item)}</TableCell>
+       </TableRow>
+       )
+    });
+
+    return items
+  }
+}
+
+export default WebShop;
--- client/src/containers/WebShop.js
+++ client/src/containers/WebShop.js
@@ -4,81 +4,64 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import ListItem from '../components/shop/ListItem'
 import { Query } from "react-apollo";
 import { GET_ITEMS } from '../graphql/queries'
 import SUBSCRIBE_ITEM_UPDATE from '../graphql/subscriptions/itemUpdated'
 import SUBSCRIBE_ITEM_CREATED from '../graphql/subscriptions/itemCreated'
 
 class WebShop extends Component {
-  checkExistingId = (prev, existing) => {
-    const prevItem = prev.getItems.filter((item) => item.id !== existing.id)
-    return Object.assign({}, prev, {
-      getItems: [existing, ...prevItem]
-      }
-    );
-  }
-  render() {
-    return (
-      	<Query query={GET_ITEMS}>
-        		{({ loading, error, data, subscribeToMore }) => {
-          	if (loading) return "Loading...";
-          	if (error) return `Error! ${error.message}`;
+	subscribeItem = (subscription) => {
+		return (prev, { subscriptionData }) => {
+			if (!subscriptionData.data) return prev;
+			const nextItem = subscriptionData.data[subscription];
+			const prevItem = prev.getItems.filter((item) => item.id !== nextItem.id)
+    		return Object.assign({}, prev, {
+      		getItems: [nextItem, ...prevItem]
+			});
+		}
+	}
+	  
+  	render() {
+    	return (
+			<Query query={GET_ITEMS}>
+				{({ loading, error, data, subscribeToMore }) => {
+					if (loading) return "Loading...";
+					if (error) return `Error! ${error.message}`;
 
-            subscribeToMore({
-              document: SUBSCRIBE_ITEM_CREATED,
-              updateQuery: (prev, { subscriptionData }) => {
-                if (!subscriptionData.data) return prev;
-                const newItem = subscriptionData.data.itemCreated;
-                return this.checkExistingId(prev, newItem)
-              }
-            });
+					subscribeToMore({
+					document: SUBSCRIBE_ITEM_CREATED,
+					updateQuery: this.subscribeItem('itemCreated')
+					});
 
-            subscribeToMore({
-              document: SUBSCRIBE_ITEM_UPDATE,
-              updateQuery: (prev, { subscriptionData }) => {
-                if (!subscriptionData.data) return prev;
-                const updatedItem = subscriptionData.data.itemUpdated;
-                return this.checkExistingId(prev, updatedItem)
-              },
-            });
-    
-          	const items = data.getItems.map((item) => {
-              let Cat = '';
-              if(item.category === 'RIDER'){Cat = 'Ryttare'}
-              else if (item.category === 'HORSE'){Cat = 'Häst'}
-              else { Cat = 'Övrigt'}
-            	return (
-                <TableRow key={item.EAN}>
-              		<TableCell>{item.name}</TableCell>
-						      <TableCell align="left">{item.color}</TableCell>
-                  <TableCell align="left">{item.description}</TableCell>
-                  <TableCell align="center">{item.quantity}</TableCell>
-            		  <TableCell align="center">{item.prize}</TableCell>
-               	  <TableCell align="right">{Cat}</TableCell>
-               </TableRow>
-               )
-            });
-          return (
-            <Table className="table-style">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Namn</TableCell>
-						<TableCell align="left">Color</TableCell>
-                  <TableCell align="left">Beskrivning</TableCell>
-                  <TableCell align="center">Lagersaldo</TableCell>
-                  <TableCell align="center">Pris</TableCell>
-                  <TableCell align="right">Kategori</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {items ? items : 'Shoppen är tyvärr tom'}
-              </TableBody>
-            </Table>
-          );
-        }}
-      </Query>
-    );
-  }
+					subscribeToMore({
+					document: SUBSCRIBE_ITEM_UPDATE,
+					updateQuery: this.subscribeItem('itemUpdated')
+					});
+
+					const items = data.getItems
+					const listItem = <ListItem list={items}  />
+					return (
+						<Table className="table-style">
+						<TableHead>
+							<TableRow>
+								<TableCell>Namn</TableCell>
+								<TableCell align="left">Color</TableCell>
+								<TableCell align="left">Beskrivning</TableCell>
+								<TableCell align="center">Lagersaldo</TableCell>
+								<TableCell align="center">Pris</TableCell>
+								<TableCell align="right">Kategori</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{items ? listItem : 'Shoppen är tyvärr tom'}
+						</TableBody>
+						</Table>
+					);
+				}}
+			</Query>
+    	);
+  	}
 }
 
 export default WebShop;
