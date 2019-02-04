9cea1aa427093cf535c8ce574ab14528a4358ec5
Malin Hanak <malinehanak@gmail.com>
2019-02-04 12:30:52 +0100

    Basic style changed

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -2,38 +2,25 @@ import React, { Component } from 'react';
 import Navigation from './Navigation';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-
-
 class Home extends Component {
   render() {
+    console.log(this.props)
     return (
-      <div className="basic-grid">
-        <article>Horse Galore <FontAwesomeIcon className="menu-options" icon="angle-down" /></article>
+      <section className="basic-grid">
         <div className="top-level">
-          Some page headline!
+          <article className="business">Horse Galore <FontAwesomeIcon className="menu-options" icon="angle-down" /></article>
           <input className="main-search" type="text" name="main-searchbar" placeholder="Sökfält..." />
         </div>
         <Navigation />
         <div className="main-content">
-          <div className="content-box">
-            {this.props.children}
-          </div>
+          {this.props.children}
         </div>
         <div className="footer">
-          <p>Horse Galore, copyright 2019</p>
+          <div>
+            <p>Horse Galore, copyright 2019</p>
+          </div>
         </div>
-        <div className="side-nav-top">
-          <FontAwesomeIcon id="menu-icon" icon="user" />
-        </div>
-        <div className="side-nav">
-          {/* This should be refactored into a component on it own, rendering all user icons for this side
-              nav view. The whole side-nav might also become a stand alone componen. */}
-          <FontAwesomeIcon className="side-menu-icon" icon="home" title="Stallet" />
-          <FontAwesomeIcon className="side-menu-icon" icon="dollar-sign" title="Banken" />
-          <FontAwesomeIcon className="side-menu-icon bottom" icon="envelope" title="Meddelanden" />
-          <FontAwesomeIcon className="side-menu-icon" icon="cog" />
-        </div>
-      </div>
+      </section>
       
     )
   }
