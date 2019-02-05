a534ec77677061024792bda35ae6ec2c235d2d2e
Malin Hanak <malinehanak@gmail.com>
2019-02-05 13:31:13 +0100

    Webshop refactored to use a HOC

--- client/src/containers/WebShop.js
+++ client/src/containers/WebShop.js
@@ -1,36 +1,11 @@
 import React, { Component } from 'react';
 import ShopTable from '../components/shop/ShopTable'
-import { Query } from "react-apollo";
-import { GET_ITEMS } from '../graphql/queries';
-import { SUBSCRIBE_ITEM_UPDATED, SUBSCRIBE_ITEM_CREATED, SUBSCRIBE_ITEM_DELETED } from '../graphql/subscriptions';
-import { subscribeToItemCreatedAndUpdated, subscribeToItemDeleted } from '../store'
+import connect from '../HOC/connect';
 
 class WebShop extends Component {  
   	render() {
 			const { match, history, location } = this.props
     	return (
-			<Query query={GET_ITEMS}>
-				{({ loading, error, data, subscribeToMore }) => {
-					if (loading) return "Loading...";
-					if (error) return `Error! ${error.message}`;
-
-					subscribeToMore({
-					document: SUBSCRIBE_ITEM_CREATED,
-					updateQuery: subscribeToItemCreatedAndUpdated('itemCreated')
-					});
-
-					subscribeToMore({
-					document: SUBSCRIBE_ITEM_UPDATED,
-					updateQuery: subscribeToItemCreatedAndUpdated('itemUpdated')
-					});
-
-					subscribeToMore({
-						document: SUBSCRIBE_ITEM_DELETED,
-						updateQuery: subscribeToItemDeleted('itemDeleted')
-					});
-
-					const items = data.getItems
-					return (
 						<div className="content-box">
 							<h3 className="title cat-title">Kategorier</h3>
 							<article className="category cat-1">
@@ -46,13 +21,10 @@ class WebShop extends Component {
 								<div className="category-name">Övrigt</div>
 							</article>
 							<h3 className="title shop-title">Webshop</h3>
-							<ShopTable payload={items} router={{history, match, location}}  />
+							<ShopTable payload={this.props.data} router={{history, match, location}}  />
 						</div>
-					);
-				}}
-			</Query>
     	);
   	}
 }
 
-export default WebShop;
+export default connect(WebShop);
