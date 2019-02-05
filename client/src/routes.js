57b10cca89f1e9c1961978f8e5c1940ef54c44d4
Malin Hanak <malinehanak@gmail.com>
2019-02-05 13:23:17 +0100

    temporary edit path

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,16 +1,18 @@
 import * as React from 'react';
 import { Route, Switch } from "react-router-dom";
 
+import EditItem from './components/EditItem';
 import NoMatch from './containers/NoMatch';
 import WebShop from './containers/WebShop';
 import Home from './containers/Home';
 
 const routes = (
-    <Switch>
-        <Route exact={true} path="/" component={Home}/>
-        <Route path="/webshop" component={WebShop}/>
-        <Route component={NoMatch}/>
-    </Switch>
+   <Switch>
+      <Route exact={true} path="/" component={Home}/>
+     <Route path="/webshop" component={WebShop}/>
+     <Route path={`/edit/:id`} component={EditItem}/>
+     <Route component={NoMatch}/>
+   </Switch>
 )
 
 export default routes
