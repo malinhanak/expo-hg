16bfc2cd794e934326f768498eec06a8b57e8f8b
Malin Hanak <malinehanak@gmail.com>
2019-02-07 15:15:13 +0100

    cart methods added

--- client/src/store/shop.js
+++ client/src/store/shop.js
@@ -1,3 +1,4 @@
+
 export const getItemCategory = (item) => {
   let category = '';
   switch(item.category) {
@@ -14,4 +15,64 @@ export const getItemCategory = (item) => {
       category = 'Övrigt'
   }
   return category
+}
+
+export const handleShoppingCart = {
+	KEY: 'randomUserShoppingCart',
+	cartContents: [],
+	init(){
+		let cartItems = localStorage.getItem(handleShoppingCart.KEY)
+		const fakeContent = [
+			{EAN: '121341', name: 'Ridhjälm', qty: 1, price: 20},
+			{EAN: 'njh3588', name: 'Benskydd', qty: 1, price: 90},
+		]
+
+		if(cartItems) return handleShoppingCart.cartContents = JSON.parse(cartItems)
+
+		handleShoppingCart.cartContents = fakeContent
+		handleShoppingCart.getItemCategory()
+	},
+	async getItemCategory(){
+		let cartItems = JSON.stringify(handleShoppingCart.cartContents);
+		await localStorage.setItem(handleShoppingCart.KEY, cartItems);
+	},
+	find(id){
+		return handleShoppingCart.cartContents.filter(item => item.id === id);
+	},
+	add(id, item){
+		const checkForExistingId = handleShoppingCart.find(id)
+		if(checkForExistingId) return handleShoppingCart.increase(id, 1);
+		handleShoppingCart.cartContents.push(item);
+		handleShoppingCart.getItemCategory();
+	},
+	remove(id){
+		handleShoppingCart.cartContents = handleShoppingCart.cartContents.filter(item => item.id !== id);
+		handleShoppingCart.getItemCategory();
+	},
+	increment(id){
+		console.log('shop.js (+)', id)
+		handleShoppingCart.cartContents = handleShoppingCart.cartContents.map(item=>{
+			if(item.id === id) return item.qty = item.qty++;
+			return item;
+	  	});
+	  handleShoppingCart.getItemCategory();
+	},
+	decrement(id){
+		console.log('shop.js (-)', id)
+		handleShoppingCart.cartContents = handleShoppingCart.cartContents.map(item=>{
+			if(item.id === id) return item.qty = item.qty--;
+			return item;
+		});
+		handleShoppingCart.cartContents.forEach(async item=>{
+			if(item.id === id && item.qty === 0) return await handleShoppingCart.remove(id);
+	  	});
+	  	handleShoppingCart.getItemCategory();
+	},
+	sort(){
+		// maybe later
+	},
+	checkout(){
+		handleShoppingCart.cartContents = [];
+		handleShoppingCart.sync()
+	}
 }
