fafe96c30310dcaaed4d3448dd2e1a192fe8002d
Praveena <kpraveena491@gmail.com>
2019-02-28 14:44:18 +0100

    export updated

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,3 +1,3 @@
 export {default as client} from './client';
 export { subscribeToItemCreatedAndUpdated, subscribeToItemDeleted, subscribeToUserChanges } from './subscribe';
-export { getItemCategory, handleShoppingCart } from './shop';
+export { getItemCategory } from './shop';
